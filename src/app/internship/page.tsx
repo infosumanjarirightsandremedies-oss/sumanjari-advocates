191473c2e7123a5974cb588aea0a894356e521fb
infosumanjarirightsandremedies-oss <info.sumanjarirightsandremedies@gmail.com>
2026-07-14 22:09:14 +0530

    Create page.tsx

---
+++ src/app/internship/page.tsx
@@ -0,0 +1,11 @@
+import Internship from '@/components/Internship'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+
+export default function InternshipPage() {
+  return (
+    <main className="relative min-h-screen">
+      <Internship />
+    </main>
+  )
+}
